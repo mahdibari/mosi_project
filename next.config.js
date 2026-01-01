f87eef93670fde257ac322142a64e13624e3e616
mahdibari <mehdi.eblis.82@gmail.com>
2026-01-01 15:26:34 +0100

    Updated

--- next.config.js
+++ next.config.js
@@ -7,6 +7,17 @@ const nextConfig = {
       'images.unsplash.com'
     ],
   },
+  remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'gbdyflffztuujzzabobv.supabase.co' ,// آدرس دامنه سوپابیس خود را اینجا قرار دهید
+        port: '',
+        pathname: '/storage/v1/object/**',
+      },
+      // اگر عکس‌ها از جای دیگر هم می‌آیند اینجا اضافه کنید
+    ],
 };
 
 module.exports = nextConfig;
+
+
